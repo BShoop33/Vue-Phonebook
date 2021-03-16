f8920fd1d350441bea89c1abfe585ba904d8042c
Braxton Shoop <braxton.shoop@gmail.com>
2021-03-16 14:32:47 -0500

    adding contact form component completed

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3 class="teal--text">New Contact</h3>
+    <v-form></v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        firstName: "",
+        lastName: "",
+        phone: "",
+        type: "",
+        email: "",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
